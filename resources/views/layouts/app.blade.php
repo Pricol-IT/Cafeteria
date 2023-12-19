1dbc627aaedde111b39d841745ecfe2b04182ad2
Suresh P <pcs.tmp2@mypricol.com>
2023-12-19 16:48:18 +0530

    Cafeteria app setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
+  <meta name="theme-color" content="#0062bc"/>
+  <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+  <link rel="manifest" href="{{ asset('/manifest.json') }}">
   <title>@yield('title') - {{ config('app.name') }}</title>
   <!-- Favicons -->
   <!-- <link href="assets/img/favicon.png" rel="icon">
@@ -33,6 +35,14 @@
   <!-- Vendor JS Files -->
 @include('parts.script')
 @yield('script')
+<script src="{{ asset('/sw.js') }}"></script>
+<script>
+    if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register("/sw.js").then(function (reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+</script>
 </body>
 
 </html>
